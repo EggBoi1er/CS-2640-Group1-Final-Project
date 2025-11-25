495d8a2d88ce556e54d4572aa92116f6f3dbbff1
alex-eng-2005 <staridoxberyl2005@gmail.com>
2025-11-24 19:17:04 -0800

    Version 1

--- SnakeGame.asm
+++ SnakeGame.asm
@@ -13,13 +13,13 @@ main:
     li $t1, 2048 # 512 by 256 pixels
     li $t2, 0xFFFFFF #Draws the background to be white
 
-repeatPix:
+bg_loop:
     sw $t2, 0(t0)
     addi, $t0, $t0, 4 #Next spot to fill in the pixels
     addi, $t1, $t1, -1 #Decrease the number of pixel of needed
     bnez, $t1, repeatPix #Repeat the pixels until there is no pixels left to fill in the background
 
-    la $t0, frameBuffer
+    la $t0, bg_loop
     li $t1, 64
     li $t2, 0x00000000
 
