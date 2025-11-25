c4bcf196588a2435216d4cff0b4e4675efca6548
Carter Do <163492013+EggBoi1er@users.noreply.github.com>
2025-11-24 22:10:22 -0800

    added snakeHeadPosition function and test for it (it works now)

--- SnakeGame.asm
+++ SnakeGame.asm
@@ -18,7 +18,7 @@ main:
 	li $t2, 0xFFFFFF #Draws the background to be white
 
 bg_loop:
-	sw $t2, 0(t0)
+	sw $t2, 0($t0)
 	addi, $t0, $t0, 4 #Next spot to fill in the pixels
 	addi, $t1, $t1, -1 #Decrease the number of pixel of needed
 	bnez, $t1, bg_loop #Repeat the pixels until there is no pixels left to fill in the background
@@ -35,12 +35,35 @@ drawBorderLeft:
 
 drawBorderRight:
 
+
+
+
 # updating the snakeHeadPosition
 # use addiu to make sure theres no overflow
 updateSnakeHeadPosition:
+	# prepare stack so it can use registers and run correctly
 	addiu $sp, $sp, -24
 	sw $fp, 0($sp)
 	sw $ra, 4($sp)
 	addiu $fp, $sp, 20
+	
+	# get the snakes current position and speed
+	lw $t3, xSpeed
+	lw $t4, ySpeed
+	lw $t5, xPos
+	lw $t6, yPos
+	
+	# move the snake head to its new position
+	add $t5, $t5, $t3
+	add $t6, $t6, $t4
 
+	# store the updated position
+	sw $t5, xPos
+	sw $t6, yPos
+	
+	# restore stack and go back to where the function was orignally called
+	lw $ra, 4($sp)
+	lw $fp, 0($sp)
+	addiu $sp, $sp, 24
+	jr $ra
 
---
+++ SnakeHeadPositionTest.asm
@@ -0,0 +1,52 @@
+# Snake Head Position
+# Only tests updateSnakeHeadPosition
+
+.data
+xPos:   .word 50 # initial x position
+yPos:   .word 50 # initial y position
+xSpeed: .word 1 # move right
+ySpeed: .word 0 # no vertical movement
+
+.text
+main:
+	# call the function 5 times to see if it increases
+	jal updateSnakeHeadPosition
+	jal updateSnakeHeadPosition
+	jal updateSnakeHeadPosition
+	jal updateSnakeHeadPosition
+	jal updateSnakeHeadPosition
+
+	# end program
+	li $v0, 10
+	syscall
+
+# updating the snakeHeadPosition
+# use addiu to make sure theres no overflow
+updateSnakeHeadPosition:
+	# prepare stack so it can use registers and run correctly
+	addiu $sp, $sp, -24
+	sw $fp, 0($sp)
+	sw $ra, 4($sp)
+	addiu $fp, $sp, 20
+	
+	# get the snakes current position and speed
+	lw $t3, xSpeed
+	lw $t4, ySpeed
+	lw $t5, xPos
+	lw $t6, yPos
+	
+	# move the snake head to its new position
+	add $t5, $t5, $t3
+	add $t6, $t6, $t4
+
+	# store the updated position
+	sw $t5, xPos
+	sw $t6, yPos
+	
+	# restore stack and go back to where the function was orignally called
+	lw $ra, 4($sp)
+	lw $fp, 0($sp)
+	addiu $sp, $sp, 24
+	jr $ra
+
+# it worked because t5 and t6 changed
